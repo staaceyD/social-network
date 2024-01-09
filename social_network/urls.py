6e5f7a8d393309d1b350840186f4743697605cf9
Anastasiia Derevianko <anastasiia1010@gmail.com>
2024-01-09 17:59:13 +0200

    add authentication

--- social_network/urls.py
+++ social_network/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from social_network.core.views import create_post
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('posts/', create_post, name='create_post')
 ]
