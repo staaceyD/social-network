0dc8d790533691c669caa7bb48581bcf4237d5ae
Anastasiia Derevianko <anastasiia1010@gmail.com>
2024-01-13 21:21:17 +0200

    add retrieve endpoint

--- social_network/core/views.py
+++ social_network/core/views.py
@@ -3,30 +3,41 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Count
-from social_network.core.models import PostLike
+from social_network.core.models import Post, PostLike
 
 from social_network.core.serializers import PostLikeAnalyticsSerializer, PostLikeSerializer, PostSerializer
 
-@api_view(["POST"])
+@api_view(["GET","POST"])
 @permission_classes([IsAuthenticated])
-def create_post(request):
-    request.data["author"] = request.user.pk
-    serializer = PostSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def posts(request):
+    if request.method == 'POST':
+        request.data["author"] = request.user.pk
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    elif request.method == 'GET':
+        user = request.user
+        posts = Post.objects.filter(author=user)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def post_like(request, post_id):
     user_id = request.user.pk
+    post = Post.objects.get(id=post_id)
+    post.likes_number +=1
     data = {"user_id": user_id, "post_id": post_id}
     serializer = PostLikeSerializer(data=data)
     if serializer.is_valid():
         serializer.save()
-        return Response(f"Post {post_id} liked successfully", status=status.HTTP_201_CREATED)
+        post.save()
+        return Response({'message':f"Post {post_id} liked successfully"}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -35,11 +46,16 @@ def post_like(request, post_id):
 def post_dislike(request, post_id):
     user_id = request.user.pk
     like_filter = PostLike.objects.filter(post_id=post_id, user_id=user_id).first()
-
+    post = Post.objects.get(id=post_id)
+    if post.likes_number > 0:
+        post.likes_number -=1
+    else:
+        return Response({'message':"The post cannot be disliked. Post was never liked"}, status=status.HTTP_400_BAD_REQUEST) 
     if like_filter:
         like_filter.delete()
-        return Response(f"Post {post_id} disliked successfully", status=status.HTTP_200_OK)
-    return Response("The post cannot be disliked. Like was not found", status=status.HTTP_400_BAD_REQUEST)
+        post.save()
+        return Response({'message':f"Post {post_id} disliked successfully"}, status=status.HTTP_200_OK)
+    return Response({'message':"The post cannot be disliked. Like was not found"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
--- social_network/urls.py
+++ social_network/urls.py
@@ -21,7 +21,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from social_network.core.views import create_post, post_dislike, post_like,post_like_analytics
+from social_network.core.views import posts, post_dislike, post_like,post_like_analytics
 from social_network.auth.views import register_user
 
 
@@ -30,7 +30,7 @@ urlpatterns = [
     path('register', register_user, name='register'),
     path('auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('posts', create_post, name='create_post'),
+    path('posts', posts, name='posts'),
     path('posts/<uuid:post_id>/likes', post_like, name='post_likes'),
     path('posts/<uuid:post_id>/dislikes', post_dislike, name='post_dislikes'),
     path('posts/likes/analytics', post_like_analytics, name='post_likes_analytics'),
