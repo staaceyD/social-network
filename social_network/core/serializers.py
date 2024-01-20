8d61d12f671942c291b018281332b90069a6a801
Anastasiia Derevianko <anastasiia1010@gmail.com>
2024-01-20 23:07:41 +0200

    add is liked field

--- social_network/core/serializers.py
+++ social_network/core/serializers.py
@@ -4,9 +4,16 @@ from .models import Post, PostLike
 
 
 class PostSerializer(serializers.ModelSerializer):
+    is_liked = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = "__all__"
+
+    def get_is_liked(self, object) -> bool:
+        return PostLike.objects.filter(
+            user_id=object.author, post_id=object.id
+        ).exists()
 
 
 class PostLikeSerializer(serializers.ModelSerializer):
